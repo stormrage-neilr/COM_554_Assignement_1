1dca01f50a3d92dd694e1a4998f79660354095dc
Neil Rafferty <neilrafferty@Neils-Air.lan>
2016-10-26 17:42:29 +0100

    fixed nav items to stay highlighted when they are selected and give hide/show content functionality to the items

--- COM554/assignment_1/javascript.js
+++ COM554/assignment_1/javascript.js
@@ -1,12 +1,37 @@
 $(document).ready(function (){
 
-    $(".nav-li").hover(function () {
-        $(this).addClass("hover");
-    }, function () {
-        $(this).removeClass("hover");
+    //Navigation button events - hide and show content.
+    $("#home-button").click(function () {
+        //Highlighting correct navigation bar button on click
+        $(".nav-li").removeClass("selected");
+        $(this).addClass("selected");
+        //Hide all sections and show the corresponding section section
+        $("section").addClass('hidden');
+        $("#home-content").removeClass('hidden');
     });
-
+    $("#seasons-button").click(function () {
+        $(".nav-li").removeClass("selected");
+        $(this).addClass("selected");
+        $("section").addClass('hidden');
+        $("#seasons-content").removeClass('hidden');
+    });
+    $("#game-button").click(function () {
+        $(".nav-li").removeClass("selected");
+        $(this).addClass("selected");
+        $("section").addClass('hidden');
+        $("#game-content").removeClass('hidden');
+    });
+    $("#members-button").click(function () {
+        $(".nav-li").removeClass("selected");
+        $(this).addClass("selected");
+        $("section").addClass('hidden');
+        $("#members-content").removeClass('hidden');
+    });
     $("#register-button").click(function () {
+        $(".nav-li").removeClass("selected");
+        $(this).addClass("selected");
+        $("section").addClass('hidden');
         $("#register-content").removeClass('hidden');
     });
+
 });
