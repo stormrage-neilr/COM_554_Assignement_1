3d80d43109c158c0a32950a79cc14f71f28facbc
Neil Rafferty <neilrafferty@Neils-Air.home>
2016-10-26 20:18:23 +0100

    Added a function to set the max dob in the registration form to the current date

--- COM554/assignment_1/javascript.js
+++ COM554/assignment_1/javascript.js
@@ -1,5 +1,7 @@
 $(document).ready(function (){
 
+    // *** Navigation bar section Functionality ***
+
     //Navigation button events - hide and show content.
     $("#home-button").click(function () {
         //Highlighting correct navigation bar button on click
@@ -8,24 +10,29 @@ $(document).ready(function (){
         //Hide all sections and show the corresponding section section
         $("section").addClass('hidden');
         $("#home-content").removeClass('hidden');
+        //Adding css to the section
+        $("#home-content").addClass('content');
     });
     $("#seasons-button").click(function () {
         $(".nav-li").removeClass("selected");
         $(this).addClass("selected");
         $("section").addClass('hidden');
         $("#seasons-content").removeClass('hidden');
+        $("#seasons-content").addClass('content');
     });
     $("#game-button").click(function () {
         $(".nav-li").removeClass("selected");
         $(this).addClass("selected");
         $("section").addClass('hidden');
         $("#game-content").removeClass('hidden');
+        $("#game-content").addClass('content');
     });
     $("#members-button").click(function () {
         $(".nav-li").removeClass("selected");
         $(this).addClass("selected");
         $("section").addClass('hidden');
         $("#members-content").removeClass('hidden');
+        $("#members-content").addClass('content');
     });
     $("#register-button").click(function () {
         $(".nav-li").removeClass("selected");
@@ -33,5 +40,24 @@ $(document).ready(function (){
         $("section").addClass('hidden');
         $("#register-content").removeClass('hidden');
     });
+
+    // *** Registration section Functionality ***
+
+    //Setting the latest date of birth in the registration form to today
+    $("#dob-input").attr("max", function(){
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        if(dd < 10){
+            dd = '0' + dd
+        }
+        if(mm < 10){
+            mm = '0' + mm
+        }
+        return today.getFullYear()+'-'+mm+'-'+dd;
+    });
+
+    //Adding the new registered member into the members list
+
 
 });
