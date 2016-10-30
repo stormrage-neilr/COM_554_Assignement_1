c9588fdb1850979d47088c2dd6c24b422d38b182
Neil Rafferty <neilrafferty@Neils-Air.lan>
2016-10-30 14:11:49 +0000

    commented and reorganised js code. Added css to members table

--- COM554/assignment_1/javascript.js
+++ COM554/assignment_1/javascript.js
@@ -41,26 +41,31 @@ $(document).ready(function (){
         $("#register-content").removeClass('hidden');
     });
 
-    // *** Members Table Functionality ***
+    /* *** Members Table Functionality ***
 
-    //Populating the members table from the xml file
-    if (document.cookie == "") {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "members.xml", false)//synchronous local get request
-        xhr.send();
-        xmlDoc = xhr.responseXML;
-    }else{
-        xmlDoc = $.parseXML(document.cookie);
+    An initial list of members is being loaded from an xml file in the project.
+    However, if the registration form is submitted by the user an updated version of
+    this xml file will be stored in a cookie. The xml file itself should be update in
+    a future release.*/
+    
+    //Retrieving an xml document from the users cookie or the xml file.
+    function setXMLDoc() {
+        if (document.cookie == "") {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "members.xml", false)//synchronous local get request.
+            xhr.send();
+            xmlDoc = xhr.responseXML;//returning xhr request as an xml document.
+        }else{
+            xmlDoc = $.parseXML(document.cookie);//returning document cookie string parsed into xml.
+        }
     }
-    var members = xmlDoc.getElementsByTagName("Member");
+    
+    //This method returns an age given a date.
+    function getAge(dob){
+        var today = new Date().getDate();
+        var thisMonth = new Date().getMonth() + 1;// +1 because months start at 0
+        var thisYear = new Date().getFullYear();
 
-    var today = new Date().getDate();
-    var thisMonth = new Date().getMonth() + 1;
-    var thisYear = new Date().getFullYear();
-
-    for(var i = 0; i< members.length; i++){
-
-        var dob = members[i].getElementsByTagName("DOB")[0].childNodes[0].nodeValue;
         var birthDay = dob.substring(8, 10);
         var birthMonth = dob.substring(5, 7);
         var birthYear = dob.substring(0, 4);
@@ -69,41 +74,56 @@ $(document).ready(function (){
         if ((birthMonth > thisMonth) || (birthMonth == thisMonth && birthDay > today)) {
             age -= 1;
         }
+        return age;
+    }
+    
+    //Populating the members table.
+    setXMLDoc();
+    var members = xmlDoc.getElementsByTagName("Member");//Retrieving xml document.
+    for(var i = 0; i< members.length; i++){
+        //Selecting the needed information from each member instance.
+        var firstname = members[i].getElementsByTagName("Firstname")[0].childNodes[0].nodeValue;
+        var surname = members[i].getElementsByTagName("Surname")[0].childNodes[0].nodeValue;
+        var dob = members[i].getElementsByTagName("DOB")[0].childNodes[0].nodeValue;
+        var email = members[i].getElementsByTagName("Email_Address")[0].childNodes[0].nodeValue;
+        var isSub = members[i].getElementsByTagName("Subscriber")[0].childNodes[0].nodeValue;
 
-        var isSubscriber;
-        if(members[i].getElementsByTagName("Subscriber")[0].childNodes[0].nodeValue === "true"){
-            isSubscriber = "Yes";
+        var name = firstname + " " + surname;
+        var age = getAge(dob);//Calculating members age.
+        var subText;
+        if(isSub === "true"){
+            subText = "Yes";
         }else{
-            isSubscriber = "No";
+            subText = "No";
         }
-        var name = members[i].getElementsByTagName("Firstname")[0].childNodes[0].nodeValue + " " + members[i].getElementsByTagName("Surname")[0].childNodes[0].nodeValue
 
+        //Inserting table row element directly into the dom with current information.
         $("#member-table-header-row").after(
             "<tr><td>" + name + "</td>" +
             "<td>" + age + "</td>" +
-            "<td>" + members[i].getElementsByTagName("Email_Address")[0].childNodes[0].nodeValue + "</td>" +
-            "<td>" + isSubscriber + "</td></tr>");
+            "<td>" + email + "</td>" +
+            "<td>" + subText + "</td></tr>");
     }
 
     // *** Registration section Functionality ***
 
-    //Setting the latest date of birth in the registration form to today
+    //Setting the latest date of birth in the registration form to today.
     $("#dob-input").attr("max", function(){
         var today = new Date();
         var dd = today.getDate();
         var mm = today.getMonth() + 1;
-        if(dd < 10){
+        if(dd < 10){//prefixing days less than 10 with 0.
             dd = '0' + dd
         }
-        if(mm < 10){
+        if(mm < 10){//prefixing months less than 10 with 0.
             mm = '0' + mm
         }
         return today.getFullYear()+'-'+mm+'-'+dd;
     });
 
-    //Storing an updated version of the xml as a string in a cookie
+    //Storing an updated version of the xml as a string in a cookie.
     $("#registration-form").submit(function() {
-
+        //Creating xml elements needed to construct an instance of a member.
         var member = xmlDoc.createElement("Member");
         var name = xmlDoc.createElement("Name");
         var firstname = xmlDoc.createElement("Firstname");
@@ -112,20 +132,29 @@ $(document).ready(function (){
         var email = xmlDoc.createElement("Email_Address");
         var subscriber = xmlDoc.createElement("Subscriber");
 
+        //Adding values from the registration form into the xml elements.
         firstname.append($('#firstname').val());
         surname.append($('#surname').val());
         dob.append($('#dob-input').val());
         email.append($('#email').val());
         subscriber.append($('#subscribe-checkbox').is(':checked'));
 
+        //adding elements into the correct parent elements.
         name.appendChild(firstname);
         name.appendChild(surname);
         member.appendChild(name);
         member.appendChild(dob);
         member.appendChild(email);
         member.appendChild(subscriber);
+
+        //Retrieving current xml document and updating it with the new member.
         xmlDoc.getElementsByTagName("Members")[0].appendChild(member);
 
+        /*
+        Saving xml document as a string into a cookie. New lines and returns were removed
+        using a regular expression as only the first line of the document was being saved.
+        Note: In a future release this should be saving to the server side.
+         */
         document.cookie = new XMLSerializer().serializeToString(xmlDoc).replace(/[\r\n]/g, '');
     });
 });
